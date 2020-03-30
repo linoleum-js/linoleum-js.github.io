8e5b7c1b545ff153341385eb9ce64267213cf3a2
m.schulz <m.schulz@artlogics.ru>
2020-03-30 13:29:33 +0500

    board

---
+++ precache-manifest.3d090f84dc75bc941d94ee19be26cb55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e824896d90fcafe5f393ca08dd9a3f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f01c3b2b10e4df36213a",
+    "url": "/static/css/main.ff928b4d.chunk.css"
+  },
+  {
+    "revision": "24e5b07fbf9246a7b413",
+    "url": "/static/js/2.e2d1c3f6.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.e2d1c3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01c3b2b10e4df36213a",
+    "url": "/static/js/main.5bcd02de.chunk.js"
+  },
+  {
+    "revision": "da2bb38ffd397ef0dfed",
+    "url": "/static/js/runtime-main.7fb4edc6.js"
+  },
+  {
+    "revision": "22b245e8d8fc9d2a11e335d5bbb03a0d",
+    "url": "/static/media/icon-add.22b245e8.svg"
+  },
+  {
+    "revision": "f9f5b268a6fc1b0ea7da2227cea2d131",
+    "url": "/static/media/icon-close.f9f5b268.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01caed657dd6759756d8a4bb1d61a47a.js"
+  "/precache-manifest.3d090f84dc75bc941d94ee19be26cb55.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
